690a884503b1ac3325e0856f64f728b73d5d1076
Nathan Conlon <nathanec@gmail.com>
2022-08-17 19:58:02 -0500

    inital work on laying out bot

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const toml = require('toml');
+
+const config = toml.parse(fs.readFileSync('./config.toml', 'utf-8'));
+const token = config.Bot.token;
+const clientId = config.Bot.clientID;
+const guildId = config.Bot.guildID;
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
